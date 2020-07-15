504df3bdcd426dc97ebd72606466e181bf7fcc0e
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 19:15:50 +0530

    EmpCheck

--- Day21EmpWageComputation.java
+++ Day21EmpWageComputation.java
@@ -2,7 +2,15 @@
 public class Day21EmpWageComputation{
 
 	public static void main(String [] args){
-	
-		System.out.println("Welcome EmpWage Computation");
+
+		int randVal = (int) (Math.random()* 2)+1;
+
+		if (randVal == 1){
+
+			System.out.println("Employee is present");
+		}else{
+
+			System.out.println("Employee is Absent");
+		}
 	}
 }

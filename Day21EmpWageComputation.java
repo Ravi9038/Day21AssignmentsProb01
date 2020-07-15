bba18c78e5e5bf99447fdf9f8de5b26b7149f225
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 19:45:12 +0530

    Switch Case

--- Day21EmpWageComputation.java
+++ Day21EmpWageComputation.java
@@ -1,32 +1,38 @@
 
 public class Day21EmpWageComputation{
 
-
 	public static final int isPartTime = 1;
 	public static final int isFullTime = 2;
 	public static final int empRatePerHr=20;
+	public static final int numOfWorkingDays=3;
 
 	public static void main(String [] args){
 
 
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 
-		int empCheck = (int) Math.floor(Math.random()*10) % 3;
+		for(int day = 0; day < numOfWorkingDays; day++){
 
-		switch(empCheck){
+			int empCheck = (int) Math.floor(Math.random()*10) % 3;
 
-			case isPartTime :
-				empHrs = 4;
-				break;
-			case isFullTime :
-				empHrs = 8;
-				break;
+			switch(empCheck){
 
-			default :
-				empHrs = 0;
+				case isPartTime :
+					empHrs = 4;
+					break;
+				case isFullTime :
+					empHrs = 8;
+					break;
+
+				default :
+					empHrs = 0;
+			}
+			empWage = empHrs * empRatePerHr;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage : "+ empWage);
 		}
-		empWage = empHrs * empRatePerHr;
-		System.out.println("Emp Wage "+ empWage);
+		System.out.println("Total Emp Wage "+ totalEmpWage);
 	}
 }

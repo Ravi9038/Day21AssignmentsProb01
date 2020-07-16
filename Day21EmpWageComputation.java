0359928fce87baafa9390a542dbad6f368defe9d
Ravi9038 <43364065+Ravi9038@users.noreply.github.com>
2020-07-16 14:36:15 +0530

    Update Day21EmpWageComputation.java

--- Day21EmpWageComputation.java
+++ Day21EmpWageComputation.java
@@ -9,30 +9,27 @@ public class Day21EmpWageComputation{
 	public static void main(String [] args){
 
 
-		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
+	int empHrs = 0;
+	int empWage = 0;
+	int totalEmpWage = 0;
 
-		for(int day = 0; day < numOfWorkingDays; day++){
 
-			int empCheck = (int) Math.floor(Math.random()*10) % 3;
+		int empCheck = (int) Math.floor(Math.random()*10) % 3;
 
-			switch(empCheck){
+		switch(empCheck){
 
-				case isPartTime :
-					empHrs = 4;
-					break;
-				case isFullTime :
-					empHrs = 8;
-					break;
+			case isPartTime :
+				empHrs = 4;
+				break;
+			case isFullTime :
+				empHrs = 8;
+				break;
 
-				default :
-					empHrs = 0;
-			}
-			empWage = empHrs * empRatePerHr;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage : "+ empWage);
+			default :
+				empHrs = 0;
 		}
-		System.out.println("Total Emp Wage "+ totalEmpWage);
+		empWage = empHrs * empRatePerHr;
+		System.out.println("Emp Wage : "+ empWage);
+
 	}
 }
